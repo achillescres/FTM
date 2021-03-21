1d252a0c5533376808c1ebabd9ff80fb0c8c1909
Aytal <aitalsemenov04@gmail.com>
2021-03-21 16:32:11 +0900

    full back

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const admin = require("firebase-admin")
+const path = require('path')
 
 const model = require('./models.js')
 const serviceAccount = require("./flytomoon-b141b-firebase-adminsdk-ys77s-a8cd02940d.json")
@@ -36,28 +37,33 @@ async function setPastures(pastures) {
     let x, y
     const newPastures = {}
     for (let i = 0; i < pastures.length; i++) {
-        x = Math.floor(pastures[i].coords.x / 11)
-        y = Math.floor(pastures[i].coords.y / 11)
-        newPastures[`${x}x${y}y`] = `${x}x${y}y`
+        x = Math.floor(Math.floor(pastures[i].coords.x / 11) * 11 / 1000)
+        y = Math.floor(Math.floor(pastures[i].coords.y / 11) * 11 / 1000)
+        let xx = Math.floor(pastures[i].coords.x / 11) * 11 / 1000
+        let yy = Math.floor(pastures[i].coords.y / 11) * 11 / 1000
+        newPastures[`${x}x${y}y`] = {coords: {x: xx , y: yy}}
     }
+
     console.log(newPastures)
     await pasturesRef.set(newPastures)
 }
 
 async function getDeers() {
-    let deers = []
-    let ndeer
-    for (let i = 0; i < 20; i++) {
-        ndeer = (await db.ref(`deers/deer${i}`).once("value")).val()
-        deers.push({
-                coords: {
-                    x: ndeer.x,
-                    y: ndeer.y
-                },
-                speed: ndeer.v, speedVector: ndeer.V
-            })
+    let _deers = (await db.ref('deers').once('value')).val()
+    const deers = []
+    for (let i in _deers) {
+        deers.push(_deers[i])
     }
     return deers
+}
+
+async function getFlags() {
+    let _flags = (await db.ref('flags').once('value')).val()
+    const flags = []
+    for (let i in _flags) {
+        flags.push(_flags[i])
+    }
+    return flags
 }
 
 async function refresher() {
@@ -68,12 +74,12 @@ async function refresher() {
 
 async function refreshPastures() {
     console.log('Refreshing...')
-    const pastures = await model.searchFlags(flags, false)
+    const pastures = await model.searchFlags(await getDeers(), false)
     await setPastures(pastures)
 }
 
 app.get('/', async (req, res) => {
-    await res.send('<h1>Hello World!</h1>')
+    res.sendfile()
 })
 
 app.post('/new', async (req, res) => {
@@ -82,11 +88,46 @@ app.post('/new', async (req, res) => {
     console.log("New data ate!")
 })
 
+function simulateDeers() {
+    let alphas = new Array(3)
+    for (let i = 0; i < alphas.length; i++) {
+        alphas[i] = {coords: {x: Math.floor(120000 + Math.random() * 25000), y: Math.floor(66000 + Math.random() * 4050)},
+            speed: 0, speedVector: {x: 0, y: 0}}
+    }
+
+    console.log(alphas)
+    let deers = {}
+    for (let i = 0; i < alphas.length; i++) {
+        deers[`deer${i}`] = alphas[i]
+    }
+
+    console.log(deers)
+    let xx
+    let k = alphas.length
+    for (let i = 0; i < alphas.length; i++) {
+        for (let j = 0; j < Number(Math.ceil(Math.random() * 3) * 3); j++) {
+            xx = Math.sqrt(Math.random())
+            deers[`deer${k}`] = {
+                    coords: {
+                        x: (xx  + alphas[i].coords.x),
+                        y: (Math.sqrt(1 - xx) + alphas[i].coords.y)
+                    }, speed: 0, speedVector: {x: 0, y: 0}
+            }
+            k++
+        }
+    }
+    db.ref('deers').set(deers)
+}
+
 app.listen(PORT, async () => {
     console.log(`Server running at ${PORT}`)
-    await refresher()
+    //await simulateDeers()
+    //await refresher()
     //setInterval(refresher, 10000)
 
     await refreshPastures()
-    setInterval(refreshPastures, 20000)
+    //setInterval(refreshPastures, 20000)
+
+    console.log(1)
+    //simulateDeers()
 })
